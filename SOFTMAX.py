9d0b6ad5976bcdfec0f7dce60b9c60689b4e4f15
kevin <1581611230@qq.com>
2017-08-13 23:06:27 +0800

    Environmental configuration is too disgusting, but finally finally finally succeeded

---
+++ SOFTMAX.py
@@ -0,0 +1,53 @@
+
+# coding: utf-8
+
+# In[33]:
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Activation
+from keras.optimizers import SGD
+
+
+# ###生成虚拟数据
+
+# In[34]:
+
+
+import numpy as np
+x_train = np.random.random((1000,20))
+y_train = keras.utils.to_categorical(np.random.randint(10,size=(1000,1)),num_classes=10)
+x_test = np.random.random((100,20))
+y_test = keras.utils.to_categorical(np.random.randint(10,size=(100,1)),num_classes=10)
+
+
+
+# #####序贯模型
+
+# In[35]:
+
+
+model = Sequential()
+
+
+# ######Dense(64)是一个64个隐藏单元的全连接层  
+# ######在第一层，你必须指定输入数据类型  
+# ######这里是20维的向量
+
+# In[36]:
+
+
+model.add(Dense(64,activation='relu',input_dim=20))
+model.add(Dropout(0.5))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10,activation='softmax'))
+
+sgd = SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
+model.compile(loss='categorical_crossentropy',
+             optimizer=sgd,
+             metrics=['accuracy'])
+model.fit(x_train,y_train,epochs=20,batch_size=128)
+score = model.evaluate(x_test,y_test,batch_size=128)
+
