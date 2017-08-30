549add610f941e0c22ef91bb420a0c4dab20b9cd
kevin <1581611230@qq.com>
2017-08-30 18:14:52 +0800

    some changes

--- Keras/classifier_from_little_data_script_3.py
+++ Keras/classifier_from_little_data_script_3.py
@@ -1,23 +1,20 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[2]:
 
 
 from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Sequential
+from keras.models import Model
 from keras.layers import Dropout, Flatten, Dense
 
-
-# In[9]:
-
-
-#模型权重文件路径
-weigths_path = '../keras/examples/vgg16_weights.h5'
-top_model_weights_path = 'fc_model.h5'
-#图片尺寸
+# path to the model weights files.
+weights_path = 'temp/vgg16_weights.h5'
+top_model_weights_path = 'temp/bottleneck_fc_model.h5'
+# dimensions of our images.
 img_width, img_height = 150, 150
 
 train_data_dir = 'data/train'
@@ -27,47 +24,43 @@ nb_validation_samples = 800
 epochs = 50
 batch_size = 16
 
-
-# In[13]:
-
-
-#建立VGG16网络
-model = applications.VGG16(weights='imagenet', include_top = False)
+# build the VGG16 network
+base_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(150,150,3))
 print('Model loaded.')
 
-#构建一个分类器模型，以摆在卷积模型的顶端
+# build a classifier model to put on top of the convolutional model
 top_model = Sequential()
-top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
 top_model.add(Dense(256, activation='relu'))
 top_model.add(Dropout(0.5))
 top_model.add(Dense(1, activation='sigmoid'))
 
-
-# In[12]:
-
-
-#注意，有必要从一个全训练的分类器开始，
-#包括顶级分类器，以便成功地进行微调
+# note that it is necessary to start with a fully-trained
+# classifier, including the top classifier,
+# in order to successfully do fine-tuning
 top_model.load_weights(top_model_weights_path)
 
-#将模型添加到卷积基础之上
-model.add(top_model)
+# add the model on top of the convolutional base
+# model.add(top_model)
+model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
 
-#将前25个层（最后一个转换块）设置为不可训练（权重不会更新）
-for layer in model.layers[:25]:
+# set the first 25 layers (up to the last conv block)
+# to non-trainable (weights will not be updated)
+for layer in model.layers[:15]:
     layer.trainable = False
 
-#用SGD /动量优化器编译模型,很慢的学习速度
-model.compile(loss='binary_crossentrop',
-             optimizer=optimizers.SGD(lr=1e-4,momentum=0.9),
-             metrics=['accuracy'])
+# compile the model with a SGD/momentum optimizer
+# and a very slow learning rate.
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
 
-#准备数据扩充配置
+# prepare data augmentation configuration
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255, #图像值设为0~1之间的书
-    shear_range=0.2, #剪切
-    zoom_range=0.2, #放大
-    horizontal_flip=True) #水平旋转
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -75,20 +68,24 @@ train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_model='binary')
+    class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_model='binary')
+    class_mode='binary')
 
-#微调模型
-model.fit_generator(train_generator,
-                   samples_per_epoch=nb_train_sampes,
-                   epochs=epochs,
-                   validation_data=validation_generator,
-                   nb_val_samples=nb_validation_samples)
+model.summary()
+
+# fine-tune the model
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size,
+    verbose=2)
 
 
 # In[ ]:
